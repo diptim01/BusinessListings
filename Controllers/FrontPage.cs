a001e9af55339931aa0cccbb05c46dcf81342a3e
Alokan Oladipo <AlokanOladipo@hotmail.com>
2021-04-24 18:31:06 +0100

    Add project files.

---
+++ Controllers/FrontPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessListings.Controllers
+{
+    public class FrontPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
